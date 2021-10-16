79fd5122bd96a0f80ce5154bf88aac6745eb5d65
kerwizzy <kerwizzy@gmail.com>
2021-10-16 14:36:13 -0400

    Improve WatchedMap init options

--- packages/dynein-watchedbuiltins/src/WatchedMap.ts
+++ packages/dynein-watchedbuiltins/src/WatchedMap.ts
@@ -4,9 +4,15 @@ import WatchedValue from "./WatchedValue.js"
 export default class WatchedMap<K, V> extends WatchedValue<Map<K, V>> {
 	readonly value: DataPort<Map<K, V>>;
 
-	constructor() {
-		super()
-		this.value = DyneinState.data(new Map())
+	constructor(iterable?: Iterable<readonly [K,V]> | null | undefined | DataPort<Map<K,V>>) {
+		super();
+		if (DyneinState.isDataPort(iterable)) {
+			this.value = iterable
+		} else if (iterable) {
+			this.value = DyneinState.data(new Map<K, V>(iterable));
+		} else {
+			this.value = DyneinState.data(new Map())
+		}
 	}
 
 	get(key: K): V | undefined {
